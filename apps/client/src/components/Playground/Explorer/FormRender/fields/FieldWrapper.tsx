9a016d701a6ca90c06eb34be19f44ef12ffeedc7
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2025-04-22 09:42:06 +0530

    refactor(client): simplify FieldWrapper component and remove unused props

--- apps/client/src/components/Playground/Explorer/FormRender/fields/FieldWrapper.tsx
+++ apps/client/src/components/Playground/Explorer/FormRender/fields/FieldWrapper.tsx
@@ -2,13 +2,7 @@ import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import { ErrorMessage } from "@hookform/error-message";
 import { useBlueprint, useDuckForm, useField } from "duck-form";
-import {
-  Fragment,
-  type PropsWithChildren,
-  useEffect,
-  useId,
-  useMemo,
-} from "react";
+import { type PropsWithChildren, useEffect, useId, useMemo } from "react";
 
 export type FieldWrapperProps = {
   label?: string;
@@ -16,11 +10,9 @@ export type FieldWrapperProps = {
   onChange?: () => void;
 };
 
-export type FieldWrapper = PropsWithChildren<{
-  className?: React.HTMLAttributes<HTMLDivElement>["className"];
-}>;
+export type FieldWrapper = PropsWithChildren;
 
-export function FieldWrapper({ className, children }: FieldWrapper) {
+export function FieldWrapper({ children }: FieldWrapper) {
   const props = useField<FieldWrapperProps>();
   const { generateId } = useDuckForm();
   const { schema } = useBlueprint();
@@ -28,10 +20,10 @@ export function FieldWrapper({ className, children }: FieldWrapper) {
   const autoId = useId();
   const customId = useMemo(
     () => generateId?.(schema, props),
-    [generateId, schema, props],
+    [generateId, schema, props]
   );
 
-  const { required, hidden, label, description, onChange } = props;
+  const { required, label, onChange } = props;
 
   const componentId = customId ?? autoId;
 
@@ -39,40 +31,20 @@ export function FieldWrapper({ className, children }: FieldWrapper) {
     onChange?.();
   }, [onChange]);
 
-  const LabelAndDescriptionWrapper =
-    label && description
-      ? ({ children }: PropsWithChildren) => <div>{children}</div>
-      : Fragment;
-
   return (
-    <div
-      className={cn(
-        hidden && "hidden",
-        "relative [&>div>div]:w-full w-full space-y-1",
-        className,
+    <div className="relative [&>div>div]:w-full w-full space-y-1">
+      {label && (
+        <Label
+          className={cn(
+            required &&
+              "after:ml-0.5 after:text-red-500 after:content-['*'] after:dark:text-red-400",
+            "leading-snug"
+          )}
+        >
+          {label}
+        </Label>
       )}
-    >
-      <div>
-        <LabelAndDescriptionWrapper>
-          {label && (
-            <Label
-              className={cn(
-                required &&
-                  "after:ml-0.5 after:text-red-500 after:content-['*'] after:dark:text-red-400",
-                "leading-snug",
-              )}
-            >
-              {label}
-            </Label>
-          )}
-          {description && (
-            <p className="text-muted-foreground dark:text-muted-foreground text-xs font-medium">
-              {description}
-            </p>
-          )}
-        </LabelAndDescriptionWrapper>
-        {children}
-      </div>
+      {children}
       <FieldErrorMessage name={componentId} />
     </div>
   );
