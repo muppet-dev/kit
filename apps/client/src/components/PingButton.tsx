7f9171871c9513966e9ee0fb4765b99fbb4314ee
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-04-24 22:11:23 -0700

    chore(client): minor changes

--- apps/client/src/components/PingButton.tsx
+++ apps/client/src/components/PingButton.tsx
@@ -1,20 +1,25 @@
 import { useConnection } from "@/providers";
 import { Bell } from "lucide-react";
 import { SidebarMenuButton } from "./ui/sidebar";
+import { useState } from "react";
+import { ConnectionStatus } from "@/hooks/use-connection";
 
 export function PingButton() {
+  // Show this loading state when the server is pinging
+  const [isLoading, setIsLoading] = useState(false);
   const { mcpClient, connectionStatus } = useConnection();
 
-  // TODO: Add a loading state
-  const onPing = () => mcpClient?.ping();
-
-  if (connectionStatus === "disconnected") return;
+  const onPing = async () => {
+    setIsLoading(true);
+    await mcpClient?.ping();
+    setIsLoading(false);
+  };
 
   return (
     <SidebarMenuButton
       onClick={onPing}
       onKeyDown={onPing}
-      disabled={connectionStatus === "error"}
+      disabled={connectionStatus !== ConnectionStatus.CONNECTED || isLoading}
     >
       <Bell />
       Ping Server
