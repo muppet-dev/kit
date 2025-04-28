e3b86f2fa0d4931ca34d2bfcb50e4bd7e5203efd
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2025-04-28 11:39:23 +0530

    refactor: simplify Tabs component layout and enhance styling

--- apps/client/src/components/Tabs/index.tsx
+++ apps/client/src/components/Tabs/index.tsx
@@ -11,15 +11,15 @@ export function Tabs() {
     <TabsPrimitive
       defaultValue={activeTool.name}
       onValueChange={(value) => changeTool(value)}
-      className="max-w-xs w-full"
+      className="w-full px-4"
     >
-      <TabsList className="w-full p-0 bg-background justify-start gap-4 text-md">
+      <TabsList className="w-full p-0 bg-background justify-start gap-4 text-md border-b rounded-none">
         {tools.map((tab) => (
           <TabsTrigger
             key={tab.name}
             value={tab.name}
             disabled={!tab.enabled}
-            className="rounded-none bg-background h-full data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary"
+            className="rounded-none bg-background h-full data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary dark:data-[state=active]:border-input/0 dark:data-[state=active]:border-b-foreground dark:data-[state=active]:bg-transparent max-w-max"
           >
             {tab.label}
           </TabsTrigger>
