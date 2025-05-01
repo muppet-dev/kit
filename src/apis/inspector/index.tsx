5754c9d4ef531c73a9b0a718724e2331541f1227
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-04-30 21:50:53 -0700

    chore: added title in head tag

--- src/apis/inspector/index.tsx
+++ src/apis/inspector/index.tsx
@@ -4,26 +4,28 @@ import { reactRenderer } from "@hono/react-renderer";
 const router = new Hono()
   .use(
     reactRenderer(({ children }) => {
-      return <html lang="en">{children}</html>;
+      return (
+        <html lang="en">
+          <head>
+            <meta charSet="utf-8" />
+            <meta
+              content="width=device-width, initial-scale=1"
+              name="viewport"
+            />
+            {import.meta.env.PROD ? (
+              <script type="module" src="/static/client.js" />
+            ) : (
+              <script type="module" src="/src/main.tsx" />
+            )}
+            <title>Muppet Inspector</title>
+          </head>
+          <body>{children}</body>
+        </html>
+      );
     }),
   )
   .get("/*", (c) => {
-    return c.render(
-      <>
-        <head>
-          <meta charSet="utf-8" />
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-          {import.meta.env.PROD ? (
-            <script type="module" src="/static/client.js" />
-          ) : (
-            <script type="module" src="/src/main.tsx" />
-          )}
-        </head>
-        <body>
-          <div id="root" />
-        </body>
-      </>,
-    );
+    return c.render(<div id="root" />);
   });
 
 export default router;
