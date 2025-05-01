8c0d63a6448f7b4848fa5144066c0797fe88fd24
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-04-30 21:47:37 -0700

    chore: minor changes

--- src/apis/inspector/index.tsx
+++ src/apis/inspector/index.tsx
@@ -2,29 +2,28 @@ import { Hono } from "hono";
 import { reactRenderer } from "@hono/react-renderer";
 
 const router = new Hono()
-	.get(
-		"*",
-		reactRenderer(({ children }) => {
-			return <html lang="en">{children}</html>;
-		}),
-	)
-	.get("/", (c) => {
-		return c.render(
-			<>
-				<head>
-					<meta charSet="utf-8" />
-					<meta content="width=device-width, initial-scale=1" name="viewport" />
-					{import.meta.env.PROD ? (
-						<script type="module" src="/static/client.js" />
-					) : (
-						<script type="module" src="/src/main.tsx" />
-					)}
-				</head>
-				<body>
-					<div id="root" />
-				</body>
-			</>,
-		);
-	});
+  .use(
+    reactRenderer(({ children }) => {
+      return <html lang="en">{children}</html>;
+    }),
+  )
+  .get("/*", (c) => {
+    return c.render(
+      <>
+        <head>
+          <meta charSet="utf-8" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          {import.meta.env.PROD ? (
+            <script type="module" src="/static/client.js" />
+          ) : (
+            <script type="module" src="/src/main.tsx" />
+          )}
+        </head>
+        <body>
+          <div id="root" />
+        </body>
+      </>,
+    );
+  });
 
 export default router;
