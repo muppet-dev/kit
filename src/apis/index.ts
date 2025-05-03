bb1ec254f630c71b46b4a62d85e1a62ce9f7b894
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-05-03 04:12:48 -0700

    feat: added support for tunneling

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,11 +2,13 @@ import { Hono } from "hono";
 import utilsRouter from "./utils";
 import inspectorRouter from "./inspector";
 import proxyRouter from "./proxy";
+import tunnelRouter from "./tunnel";
 
 const router = new Hono();
 
 router.route("/api", utilsRouter);
 router.route("/api", proxyRouter);
+router.route("/api/tunnel", tunnelRouter);
 router.route("/", inspectorRouter);
 
 export default router;
