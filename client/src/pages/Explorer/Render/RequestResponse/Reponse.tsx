0f37816fa5694cc398becae702ade2a3ddae10b7
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2025-05-08 12:34:10 +0530

    fix(client): improve loading state handling in ReponseRender component

--- client/src/pages/Explorer/Render/RequestResponse/Reponse.tsx
+++ client/src/pages/Explorer/Render/RequestResponse/Reponse.tsx
@@ -1,5 +1,5 @@
 import { CodeHighlighter } from "@/components/Hightlighter";
-import { Spinner } from "@/components/ui/spinner";
+import { Skeleton } from "@/components/ui/skeleton";
 import { numberFormatter } from "@/lib/utils";
 import { useFormContext } from "react-hook-form";
 
@@ -15,15 +15,7 @@ export function ReponseRender(props: ReponseRender) {
     formState: { isSubmitting },
   } = useFormContext();
 
-  if (isSubmitting)
-    return (
-      <div className="flex items-center h-full justify-center gap-2">
-        <Spinner />
-        Loading...
-      </div>
-    );
-
-  if (props.data == null) return <></>;
+  if (props.data == null && !isSubmitting) return <></>;
 
   return (
     <div className="h-full flex flex-col gap-2 overflow-y-auto pt-2 border-t">
@@ -31,15 +23,25 @@ export function ReponseRender(props: ReponseRender) {
         <h2 className="text-sm font-semibold">Response</h2>
         <div className="flex-1" />
         <span className="text-xs text-green-600 font-medium dark:text-green-400">
-          {props.data.duration > 1000
-            ? `${numberFormatter(
-                Number((props.data.duration / 1000).toFixed(2)),
-                "decimal"
-              )} s`
-            : `${numberFormatter(props.data.duration, "decimal")} ms`}
+          {isSubmitting ? (
+            <Skeleton className="w-14 h-5" />
+          ) : (props.data?.duration ?? 0) > 1000 ? (
+            `${numberFormatter(
+              Number(((props.data?.duration ?? 0) / 1000).toFixed(2)),
+              "decimal"
+            )} s`
+          ) : (
+            `${numberFormatter(props.data?.duration ?? 0, "decimal")} ms`
+          )}
         </span>
       </div>
-      <CodeHighlighter content={JSON.stringify(props.data.content, null, 2)} />
+      {isSubmitting ? (
+        <Skeleton className="size-full" />
+      ) : (
+        <CodeHighlighter
+          content={JSON.stringify(props.data?.content, null, 2)}
+        />
+      )}
     </div>
   );
 }
