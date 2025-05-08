43d5272b8520e4e5131fbfdf866bd4551c5f33ba
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2025-05-08 10:35:06 +0530

    feat(client): add Header Name field in SSE Optional Field

--- client/src/components/ConfigForm/OptionalFields/SSE.tsx
+++ client/src/components/ConfigForm/OptionalFields/SSE.tsx
@@ -28,13 +28,21 @@ export function SSEFields() {
           <AccordionTrigger className="hover:no-underline cursor-pointer hover:bg-accent/80 data-[state=open]:bg-accent/80 py-1.5 hover:px-2 data-[state=open]:px-2">
             Authentication
           </AccordionTrigger>
-          <AccordionContent className="pt-2 pb-0">
+          <AccordionContent className="pt-2 pb-0 space-y-2">
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="bearerToken">Bearer Token</Label>
               <Input
                 className="col-span-3"
-                placeholder="Enter Bearer Token"
+                placeholder="Bearer Token"
                 {...register("bearerToken")}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="headerName">Header Name</Label>
+              <Input
+                className="col-span-3"
+                placeholder="Authorization"
+                {...register("headerName")}
               />
             </div>
           </AccordionContent>
