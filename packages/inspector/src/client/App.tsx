6252109a3e4f95e61afff0da0a1ddc397fb5d842
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2025-05-10 15:02:46 +0530

    refector(inspector): shifted qeruy client provider app to main file

--- packages/inspector/src/client/App.tsx
+++ packages/inspector/src/client/App.tsx
@@ -8,36 +8,14 @@ import {
   ShikiProvider,
   useConfig,
 } from "@/client/providers";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import toast from "react-hot-toast";
+import { Transport } from "@muppet-kit/shared";
 import { BrowserRouter, Route, Routes } from "react-router";
 import ExplorerPage from "./pages/Explorer";
 import { HomePage } from "./pages/Home";
+import OAuthCallbackPage from "./pages/OAuthCallback";
 import PlaygroundPage from "./pages/Playground";
 import SettingsPage from "./pages/Settings";
 import TracingPage from "./pages/Tracing";
-import OAuthCallbackPage from "./pages/OAuthCallback";
-import { Transport } from "@muppet-kit/shared";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      throwOnError: (err, query) => {
-        console.error(query, err);
-        toast.error(err.message ?? "Something went wrong!");
-
-        return false;
-      },
-      staleTime: Number.POSITIVE_INFINITY,
-    },
-    mutations: {
-      onError: (err) => {
-        console.error(err);
-        toast.error(err.message ?? "Something went wrong!");
-      },
-    },
-  },
-});
 
 export default function App() {
   const { connectionInfo, setConnectionInfo } = useConfig();
@@ -62,24 +40,22 @@ export default function App() {
   return (
     <NotificationProvider>
       <ConnectionProvider>
-        <QueryClientProvider client={queryClient}>
-          <ShikiProvider>
-            <SidebarProvider>
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<AppWrapper />}>
-                    <Route index element={<HomePage />} />
-                    <Route path="/explorer" element={<ExplorerPage />} />
-                    <Route path="/playground" element={<PlaygroundPage />} />
-                    <Route path="/tracing" element={<TracingPage />} />
-                    <Route path="/settings" element={<SettingsPage />} />
-                  </Route>
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </BrowserRouter>
-            </SidebarProvider>
-          </ShikiProvider>
-        </QueryClientProvider>
+        <ShikiProvider>
+          <SidebarProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<AppWrapper />}>
+                  <Route index element={<HomePage />} />
+                  <Route path="/explorer" element={<ExplorerPage />} />
+                  <Route path="/playground" element={<PlaygroundPage />} />
+                  <Route path="/tracing" element={<TracingPage />} />
+                  <Route path="/settings" element={<SettingsPage />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </SidebarProvider>
+        </ShikiProvider>
       </ConnectionProvider>
     </NotificationProvider>
   );
--- packages/inspector/src/client/main.tsx
+++ packages/inspector/src/client/main.tsx
@@ -2,13 +2,36 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ConfigProvider, ThemeProvider } from "./providers";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: (err, query) => {
+        console.error(query, err);
+        toast.error(err.message ?? "Something went wrong!");
+
+        return false;
+      },
+      staleTime: Number.POSITIVE_INFINITY,
+    },
+    mutations: {
+      onError: (err) => {
+        console.error(err);
+        toast.error(err.message ?? "Something went wrong!");
+      },
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
-  <ConfigProvider>
-    <ThemeProvider>
-      <App />
-      <Toaster position="bottom-right" />
-    </ThemeProvider>
-  </ConfigProvider>,
+  <QueryClientProvider client={queryClient}>
+    <ConfigProvider>
+      <ThemeProvider>
+        <App />
+        <Toaster position="bottom-right" />
+      </ThemeProvider>
+    </ConfigProvider>
+  </QueryClientProvider>
 );
