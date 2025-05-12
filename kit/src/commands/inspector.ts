36641a2a91f39ff84b9f792d7ac3d4d6c680c9d5
Aditya Mathur <mathuraditya724@gmail.com>
2025-05-12 16:41:53 +0530

    chore: minor changes

--- kit/src/commands/inspector.ts
+++ kit/src/commands/inspector.ts
@@ -1,10 +1,28 @@
-import { Command } from "commander";
+import { Command, Option } from "commander";
 import { execa } from "execa";
 
 const command = new Command("inspector")
   .description("start the MCP Inspector")
-  .action(async () => {
-    await execa("npx", ["@muppet-kit/inspector@latest", "-y"]);
+  .addOption(
+    new Option("-p, --port <port>", "Port to run the inspector on").default(
+      "3000",
+    ),
+  )
+  .addOption(
+    new Option("-h, --host <host>", "Host to run the inspector on").default(
+      "localhost",
+    ),
+  )
+  .addOption(
+    new Option("-c, --config <config>", "Path to the config file").default(
+      "muppet.config",
+    ),
+  )
+  .action(async (options) => {
+    const _args = Object.entries(options).map(
+      ([key, value]) => `--${key} ${value}`,
+    );
+    await execa("npx", ["@muppet-kit/inspector@latest", "-y", ..._args]);
   });
 
 export default command;
--- packages/inspector/src/cli.ts
+++ packages/inspector/src/cli.ts
@@ -4,7 +4,7 @@ import {
   defineInspectorConfig,
 } from "@muppet-kit/shared";
 import { loadConfig } from "c12";
-import { program } from "commander";
+import { program, Option } from "commander";
 import pkg from "../package.json" assert { type: "json" };
 import app from "./index.js";
 
@@ -12,21 +12,40 @@ program
   .name("muppet inspector")
   .description("start the MCP Inspector")
   .version(pkg.version)
-  .action(async () => {
+  .addOption(
+    new Option("-p, --port <port>", "Port to run the inspector on").default(
+      "3000",
+    ),
+  )
+  .addOption(
+    new Option("-h, --host <host>", "Host to run the inspector on").default(
+      "localhost",
+    ),
+  )
+  .addOption(
+    new Option("-c, --config <config>", "Path to the config file").default(
+      "muppet.config",
+    ),
+  )
+  .action(async (options) => {
     console.log("Starting the Inspector...");
 
     // Load the config file
     const { config } = await loadConfig<InspectorConfig>({
       dotenv: true,
-      name: "muppet",
+      ...(options?.config
+        ? {
+            configFile: options?.config,
+          }
+        : { name: "muppet" }),
     });
 
     const _config = defineInspectorConfig(config);
 
     serve({
       fetch: app.fetch,
-      port: _config.port,
-      hostname: _config.host,
+      port: options?.port ?? _config.port,
+      hostname: options?.host ?? _config.host,
     });
 
     console.log(
