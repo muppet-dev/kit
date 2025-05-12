2c911d92fa273d062048296c8f9389af51d5de47
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2025-05-12 14:07:12 +0530

    fix: update form context to use configTransportSchema and add save functionality

--- packages/inspector/src/client/pages/Settings/Footer.tsx
+++ packages/inspector/src/client/pages/Settings/Footer.tsx
@@ -3,7 +3,11 @@ import { Spinner } from "@/client/components/ui/spinner";
 import { eventHandler } from "@/client/lib/eventHandler";
 import { useConfig, useConnection } from "@/client/providers";
 import { ConnectionStatus } from "@/client/providers/connection/manager";
-import type { transportSchema } from "@/client/validations";
+import {
+  DocumentSubmitType,
+  SUBMIT_BUTTON_KEY,
+  type configTransportSchema,
+} from "@/client/validations";
 import _ from "lodash";
 import { useFormContext, useWatch } from "react-hook-form";
 import type z from "zod";
@@ -11,13 +15,18 @@ import type z from "zod";
 export function FormFooter() {
   const { connectionInfo } = useConfig();
   const { connectionStatus } = useConnection();
-  const { reset, control } = useFormContext<z.infer<typeof transportSchema>>();
+  const { reset, control, setValue } =
+    useFormContext<z.infer<typeof configTransportSchema>>();
 
   const formValues = useWatch({ control });
   const isSameValues = _.isEqual(formValues, connectionInfo);
   const isConnecting = connectionStatus === ConnectionStatus.CONNECTING;
 
   const handleResetForm = eventHandler(() => reset(connectionInfo));
+
+  const handleSaveAndAddAnother = () => {
+    setValue(SUBMIT_BUTTON_KEY, DocumentSubmitType.SAVE_AND_CONNECT);
+  };
 
   return (
     <div className="flex items-center justify-between">
@@ -30,7 +39,12 @@ export function FormFooter() {
       >
         Reset
       </Button>
-      <Button type="submit" disabled={isSameValues || isConnecting}>
+      <Button
+        type="submit"
+        disabled={isSameValues || isConnecting}
+        onClick={handleSaveAndAddAnother}
+        onKeyDown={handleSaveAndAddAnother}
+      >
         {isConnecting && <Spinner className="size-4 min-w-4 min-h-4" />}
         {isConnecting ? "Connecting" : "Save & connect"}
       </Button>
