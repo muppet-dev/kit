82c203ba0bc403e016512e910f7301d74fa907ba
Aditya Mathur <mathuraditya724@gmail.com>
2025-05-15 16:04:17 +0530

    chore: minor changes

--- packages/inspector/src/client/pages/Explorer/Render/Executor/JSONRender.tsx
+++ packages/inspector/src/client/pages/Explorer/Render/Executor/JSONRender.tsx
@@ -1,9 +1,71 @@
 import { CodeHighlighter } from "@/client/components/Hightlighter";
 
 export type JSONRender = {
-  content: unknown;
+  content: {
+    content: { type: "text"; text: string }[];
+  };
 };
 
 export function JSONRender(props: JSONRender) {
-  return <CodeHighlighter content={JSON.stringify(props.content, null, 2)} />;
+  return (
+    <>
+      {props.content.content.map((item, index) => {
+        if (item.type === "text") {
+          return <TextRender key={`item-${index}`} content={item.text} />;
+        }
+        return <p key={`item-${index}`}>Unable to parse value</p>;
+      })}
+    </>
+  );
+}
+
+function TextRender({ content }: { content: string }) {
+  const { success, data } = tryParseJson(content);
+
+  return (
+    <CodeHighlighter
+      content={success ? JSON.stringify(data, null, 2) : String(data)}
+    />
+  );
+}
+
+export type JsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | JsonValue[]
+  | { [key: string]: JsonValue };
+
+export type JsonObject = { [key: string]: JsonValue };
+
+export type DataType =
+  | "string"
+  | "number"
+  | "bigint"
+  | "boolean"
+  | "symbol"
+  | "undefined"
+  | "object"
+  | "function"
+  | "array"
+  | "null";
+
+export function tryParseJson(str: string): {
+  success: boolean;
+  data: JsonValue;
+} {
+  const trimmed = str.trim();
+  if (
+    !(trimmed.startsWith("{") && trimmed.endsWith("}")) &&
+    !(trimmed.startsWith("[") && trimmed.endsWith("]"))
+  ) {
+    return { success: false, data: str };
+  }
+  try {
+    return { success: true, data: JSON.parse(str) };
+  } catch {
+    return { success: false, data: str };
+  }
 }
