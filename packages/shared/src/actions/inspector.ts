e657a500b2eaffab621b86cfc1964bd424063a64
Aditya Mathur <mathuraditya724@gmail.com>
2025-05-22 12:55:08 +0530

    feat: added option to auto open

--- packages/shared/src/actions/inspector.ts
+++ packages/shared/src/actions/inspector.ts
@@ -2,6 +2,7 @@ import { serve } from "@hono/node-server";
 import { loadConfig } from "c12";
 import type { Hono } from "hono";
 import type { SanitizedInspectorConfig } from "../types";
+import open from "open";
 
 type InspectorActionOptions = {
   options?: Record<string, string>;
@@ -42,7 +43,13 @@ export async function inspectorAction({
     hostname: config.host,
   });
 
-  console.log(
-    `Inspector is up and running at http://${config.host}:${config.port}`,
-  );
+  const url = `http://${config.host}:${config.port}`;
+
+  console.log(`Inspector is up and running at ${url}`);
+
+  if (config.auto_open) {
+    await open(url, {
+      wait: false,
+    });
+  }
 }
