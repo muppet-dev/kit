060a2b189152b9e3ea42d1f66437f5e3a5fa4a68
Aditya Mathur <mathuraditya724@gmail.com>
2025-05-24 11:02:14 +0530

    chore: minor changes

--- packages/proxy/src/routes/index.ts
+++ packages/proxy/src/routes/index.ts
@@ -1,10 +1,21 @@
 import { Hono } from "hono";
 import statsRouter from "./stats";
 import serversRouter from "./servers";
+import pkg from "../../package.json";
+import clientRouter from "./client";
+
+const apiRouter = new Hono();
+
+apiRouter.route("/stats", statsRouter);
+apiRouter.route("/servers", serversRouter);
 
 const router = new Hono();
 
-router.route("/stats", statsRouter);
-router.route("/servers", serversRouter);
+router.route("/api", apiRouter);
+router.get("/version", (c) => c.text(pkg.version));
+
+if (import.meta.env.DEV) {
+  router.route("/", clientRouter);
+}
 
 export default router;
