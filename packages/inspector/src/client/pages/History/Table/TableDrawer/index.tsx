04fd1b2617a79bd6774816fc527d6e74d241e71f
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2025-06-13 10:46:12 +0530

    feat: implement formatted response in the history and tracing

--- packages/inspector/src/client/pages/History/Table/TableDrawer/index.tsx
+++ packages/inspector/src/client/pages/History/Table/TableDrawer/index.tsx
@@ -1,4 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { FormattedResponseDialog } from "@/client/components/FormattedResponseDialog";
+import { EmptyResultSchema } from "@modelcontextprotocol/sdk/types.js";
+import {
+  ChevronDown,
+  ChevronUp,
+  ExternalLink,
+  RefreshCcw,
+  X,
+} from "lucide-react";
+import { type BaseSyntheticEvent, useState } from "react";
 import { CodeHighlighter } from "../../../../components/Hightlighter";
 import { Button } from "../../../../components/ui/button";
 import {
@@ -10,9 +20,6 @@ import { eventHandler } from "../../../../lib/eventHandler";
 import { cn } from "../../../../lib/utils";
 import { useConnection } from "../../../../providers";
 import type { RequestHistory } from "../../../../providers/connection/manager";
-import { EmptyResultSchema } from "@modelcontextprotocol/sdk/types.js";
-import { ChevronDown, ChevronUp, RefreshCcw, X } from "lucide-react";
-import { useState } from "react";
 import { useHistory } from "../../providers";
 import { UpdateRequestDialog } from "./UpdateRequestDialog";
 
@@ -36,6 +43,7 @@ export function TableDrawer({ traces }: TableDrawer) {
     Record<string, boolean | undefined>
   >({});
   const { selected, setSelected } = useHistory();
+  const [isOpenResponseDialog, setOpenResponseDialog] = useState(false);
 
   const selectedHistory =
     selected != null ? traces.find((item) => item.id === selected) : null;
@@ -102,133 +110,180 @@ export function TableDrawer({ traces }: TableDrawer) {
 
   const handleCloseDrawer = eventHandler(() => setSelected(null));
 
+  const handleOpenResponseDialog = eventHandler(() =>
+    setOpenResponseDialog(true)
+  );
+
   const selectedIndex = traces.findIndex((item) => item.id === selected);
+  const method = selectedHistory.request?.method;
+
+  const isResponseMethodAvailable =
+    method === "tools/call" ||
+    method === "prompts/get" ||
+    method === "resources/read";
 
   return (
-    <div className="p-4 w-[550px] border rounded-lg h-full flex overflow-y-auto">
-      <div className="flex flex-col gap-3 size-full overflow-y-auto">
-        <div className="flex items-center gap-2">
-          <kbd className="text-foreground rounded bg-secondary border px-1.5 text-sm font-medium shadow">
-            {selectedHistory.request.method}
-          </kbd>
-          <Tooltip>
-            <TooltipTrigger>
-              <div
-                className={cn(
-                  "rounded-full size-[7px] min-w-[7px] min-h-[7px]",
-                  selectedHistory.response?.error
-                    ? "bg-destructive"
-                    : "bg-success"
-                )}
-              />
-            </TooltipTrigger>
-            <TooltipContent>
-              {selectedHistory.response?.error ? "Error" : "Success"}
-            </TooltipContent>
-          </Tooltip>
-          <div className="flex-1" />
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                size="icon"
-                variant="ghost"
-                className="p-1 size-max"
-                disabled={selected != null && selectedIndex === 0}
-                onClick={handleGoToPreviosRequest}
-                onKeyDown={handleGoToPreviosRequest}
-              >
-                <ChevronUp />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>Previous request</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                size="icon"
-                variant="ghost"
-                className="p-1 size-max"
-                disabled={
-                  selected != null && selectedIndex === traces.length - 1
-                }
-                onClick={handleGoToNextRequest}
-                onKeyDown={handleGoToNextRequest}
-              >
-                <ChevronDown />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>Next request</TooltipContent>
-          </Tooltip>
-          <div className="h-4 w-px bg-muted" />
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                size="icon"
-                variant="ghost"
-                className="p-1 size-max"
-                onClick={handleCloseDrawer}
-                onKeyDown={handleCloseDrawer}
-              >
-                <X />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>Close</TooltipContent>
-          </Tooltip>
-        </div>
-        {selectedHistory.request.method !== "initialize" && (
-          <div className="flex items-center justify-end gap-2">
+    <>
+      <div className="p-4 w-[550px] border rounded-lg h-full flex overflow-y-auto">
+        <div className="flex flex-col gap-3 size-full overflow-y-auto">
+          <div className="flex items-center gap-2">
+            <kbd className="text-foreground rounded bg-secondary border px-1.5 text-sm font-medium shadow">
+              {method}
+            </kbd>
+            <Tooltip>
+              <TooltipTrigger>
+                <div
+                  className={cn(
+                    "rounded-full size-[7px] min-w-[7px] min-h-[7px]",
+                    selectedHistory.response?.error
+                      ? "bg-destructive"
+                      : "bg-success"
+                  )}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                {selectedHistory.response?.error ? "Error" : "Success"}
+              </TooltipContent>
+            </Tooltip>
+            <div className="flex-1" />
             <Tooltip>
               <TooltipTrigger asChild>
                 <Button
                   size="icon"
                   variant="ghost"
-                  className="p-1.5 size-max"
-                  onClick={handleSendRequest}
-                  onKeyDown={handleSendRequest}
-                  disabled={resendDirectory[selectedHistory.id]}
+                  className="p-1 size-max"
+                  disabled={selected != null && selectedIndex === 0}
+                  onClick={handleGoToPreviosRequest}
+                  onKeyDown={handleGoToPreviosRequest}
                 >
-                  <RefreshCcw
-                    className={
-                      resendDirectory[selectedHistory.id]
-                        ? "animate-spin"
-                        : undefined
-                    }
-                  />
+                  <ChevronUp />
                 </Button>
               </TooltipTrigger>
-              <TooltipContent>Resend request</TooltipContent>
+              <TooltipContent>Previous request</TooltipContent>
             </Tooltip>
-            {UPDATABLE_METHODS.includes(selectedHistory.request.method) && (
-              <UpdateRequestDialog request={selectedHistory.request} />
-            )}
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  className="p-1 size-max"
+                  disabled={
+                    selected != null && selectedIndex === traces.length - 1
+                  }
+                  onClick={handleGoToNextRequest}
+                  onKeyDown={handleGoToNextRequest}
+                >
+                  <ChevronDown />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Next request</TooltipContent>
+            </Tooltip>
+            <div className="h-4 w-px bg-muted" />
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  className="p-1 size-max"
+                  onClick={handleCloseDrawer}
+                  onKeyDown={handleCloseDrawer}
+                >
+                  <X />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Close</TooltipContent>
+            </Tooltip>
           </div>
-        )}
-        <TracingDetails
-          label="Request"
-          content={JSON.stringify(selectedHistory.request, null, 2)}
-        />
-        {Object.values(selectedHistory.response ?? {}).map((res) => res != null)
-          .length > 0 && (
+          {method !== "initialize" && (
+            <div className="flex items-center justify-end gap-2">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    size="icon"
+                    variant="ghost"
+                    className="p-1.5 size-max"
+                    onClick={handleSendRequest}
+                    onKeyDown={handleSendRequest}
+                    disabled={resendDirectory[selectedHistory.id]}
+                  >
+                    <RefreshCcw
+                      className={
+                        resendDirectory[selectedHistory.id]
+                          ? "animate-spin"
+                          : undefined
+                      }
+                    />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Resend request</TooltipContent>
+              </Tooltip>
+              {UPDATABLE_METHODS.includes(method) && (
+                <UpdateRequestDialog request={selectedHistory.request} />
+              )}
+            </div>
+          )}
           <TracingDetails
-            label="Response"
-            content={JSON.stringify(selectedHistory.response, null, 2)}
+            label="Request"
+            content={JSON.stringify(selectedHistory.request, null, 2)}
+          />
+          {Object.values(selectedHistory.response ?? {}).map(
+            (res) => res != null
+          ).length > 0 && (
+            <TracingDetails
+              label="Response"
+              content={JSON.stringify(selectedHistory.response, null, 2)}
+              onClick={handleOpenResponseDialog}
+              showButton={isResponseMethodAvailable}
+            />
+          )}
+        </div>
+      </div>
+      {isOpenResponseDialog &&
+        isResponseMethodAvailable &&
+        selectedHistory.response && (
+          <FormattedResponseDialog
+            response={selectedHistory.response}
+            onOpenChange={setOpenResponseDialog}
+            open={isOpenResponseDialog}
           />
         )}
-      </div>
-    </div>
+    </>
   );
 }
+
+type TracingDetails = {
+  label: string;
+  content: string;
+  onClick?: (event: BaseSyntheticEvent) => void;
+  showButton?: boolean;
+};
 
 function TracingDetails({
   label,
   content,
-}: {
-  label: string;
-  content: string;
-}) {
+  onClick,
+  showButton,
+}: TracingDetails) {
   return (
     <div className="flex-1 flex flex-col gap-1 overflow-y-auto">
-      <h2 className="text-sm font-semibold">{label}</h2>
+      <div className="flex items-center justify-between">
+        <h2 className="text-sm font-semibold">{label}</h2>
+        {showButton && (
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                onClick={onClick}
+                onKeyDown={onClick}
+                variant="ghost"
+                className="size-max has-[>svg]:px-1.5 py-1.5"
+              >
+                <ExternalLink />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>View Response</TooltipContent>
+          </Tooltip>
+        )}
+      </div>
       <CodeHighlighter content={content} className="max-h-full" />
     </div>
   );
